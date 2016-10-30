9dda254021ff702c36d5cbe214b8551921fea94e
Sergey <pincats@yandex.ru>
2016-10-30 17:33:15 +0300

    Added possibility to run tests in different browsers

--- addressbook-web-tests/src/test/java/ru/pincats/jpt/addressbook/appmanager/ApplictionManager.java
+++ addressbook-web-tests/src/test/java/ru/pincats/jpt/addressbook/appmanager/ApplictionManager.java
@@ -1,6 +1,10 @@
 package ru.pincats.jpt.addressbook.appmanager;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
 
 import java.util.concurrent.TimeUnit;
 
@@ -9,15 +13,26 @@ import java.util.concurrent.TimeUnit;
  */
 public class ApplictionManager {
 
-    FirefoxDriver wd;
+    WebDriver wd;
 
     private GroupHelper groupHelper;
     private ContactHelper contactHelper;
     private NavigationHelper navigationHelper;
     private SessionHelper sessionHelper;
+    private String browser;
+
+    public ApplictionManager(String browser) {
+        this.browser = browser;
+    }
 
     public void init() {
-        wd = new FirefoxDriver();
+        if (browser == BrowserType.FIREFOX) {
+            wd = new FirefoxDriver();
+        } else if (browser == BrowserType.CHROME) {
+            wd = new ChromeDriver();
+        } else if (browser == BrowserType.IE) {
+            wd = new InternetExplorerDriver();
+        }
         wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
         groupHelper = new GroupHelper(wd);
         contactHelper = new ContactHelper(wd);
--- addressbook-web-tests/src/test/java/ru/pincats/jpt/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/pincats/jpt/addressbook/tests/TestBase.java
@@ -1,5 +1,6 @@
 package ru.pincats.jpt.addressbook.tests;
 
+import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import ru.pincats.jpt.addressbook.appmanager.ApplictionManager;
@@ -9,7 +10,7 @@ import ru.pincats.jpt.addressbook.appmanager.ApplictionManager;
  */
 public class TestBase {
 
-    protected final ApplictionManager app = new ApplictionManager();
+    protected final ApplictionManager app = new ApplictionManager(BrowserType.CHROME);
 
     @BeforeMethod
     public void setUp() throws Exception {
