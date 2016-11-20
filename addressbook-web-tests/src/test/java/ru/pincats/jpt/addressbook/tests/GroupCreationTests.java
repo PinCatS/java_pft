c51fb57e7e63ce689dda3d034f04331cf8436471
Sergey <pincats@yandex.ru>
2016-11-20 14:15:59 +0300

    Data provider added for GroupCreationTests

--- addressbook-web-tests/src/test/java/ru/pincats/jpt/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/pincats/jpt/addressbook/tests/GroupCreationTests.java
@@ -1,19 +1,33 @@
 package ru.pincats.jpt.addressbook.tests;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.pincats.jpt.addressbook.model.GroupData;
 import ru.pincats.jpt.addressbook.model.Groups;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GroupCreationTests extends TestBase {
 
-    @Test
-    public void testGroupCreation() {
+    @DataProvider
+    public Iterator<Object[]> validGroups() {
+        List<Object[]> list = new ArrayList<>();
+        list.add(new Object[] { new GroupData().withName("test 1").withHeader("test header 1").withFooter("test footer 1")});
+        list.add(new Object[] { new GroupData().withName("test 2").withHeader("test header 2").withFooter("test footer 2")});
+        list.add(new Object[] { new GroupData().withName("test 3").withHeader("test header 3").withFooter("test footer 3")});
+        return list.iterator();
+    }
+
+    @Test(dataProvider = "validGroups")
+    public void testGroupCreation(GroupData group) {
         app.goTo().groupPage();
         Groups before = app.group().all();
-        GroupData group = new GroupData().withName("test1");
+        //GroupData group = new GroupData().withName("test1");
         app.group().create(group);
         assertThat(app.group().count(), equalTo(before.size() + 1));
         Groups after = app.group().all();
