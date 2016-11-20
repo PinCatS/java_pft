feb638ada2b9cf0728cebef745c03c05e50a60da
Sergey <pincats@yandex.ru>
2016-11-20 16:03:14 +0300

    Add json format support for GroupDataGenerator
    + GroupCreationTests was adjusted to read from json

--- addressbook-web-tests/src/test/java/ru/pincats/jpt/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/pincats/jpt/addressbook/tests/GroupCreationTests.java
@@ -1,5 +1,8 @@
 package ru.pincats.jpt.addressbook.tests;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
 import com.thoughtworks.xstream.XStream;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
@@ -48,7 +51,23 @@ public class GroupCreationTests extends TestBase {
         return groups.stream().map((g) -> new Object[] {g}).collect(Collectors.toList()).iterator();
     }
 
-    @Test(dataProvider = "validGroupsFromXml")
+    @DataProvider
+    public Iterator<Object[]> validGroupsFromJson() throws IOException {
+        List<Object[]> list = new ArrayList<>();
+        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.json")));
+        String json = "";
+        String line = reader.readLine();
+        while (line != null) {
+            json += line;
+            line = reader.readLine();
+        }
+        reader.close();
+        Gson gson = new Gson();
+        List<GroupData> groups = gson.fromJson(json, new TypeToken<List<GroupData>>(){}.getType());
+        return groups.stream().map((g) -> new Object[] {g}).collect(Collectors.toList()).iterator();
+    }
+
+    @Test(dataProvider = "validGroupsFromJson")
     public void testGroupCreation(GroupData group) {
         app.goTo().groupPage();
         Groups before = app.group().all();
