834847dd4912a7d16f1ddbb9f81a40cd3780bc1b
Sergey <pincats@yandex.ru>
2016-11-20 14:30:22 +0300

    Passing group data from groups.csv to the test

--- addressbook-web-tests/src/test/java/ru/pincats/jpt/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/pincats/jpt/addressbook/tests/GroupCreationTests.java
@@ -5,6 +5,7 @@ import org.testng.annotations.Test;
 import ru.pincats.jpt.addressbook.model.GroupData;
 import ru.pincats.jpt.addressbook.model.Groups;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -15,11 +16,16 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class GroupCreationTests extends TestBase {
 
     @DataProvider
-    public Iterator<Object[]> validGroups() {
+    public Iterator<Object[]> validGroups() throws IOException {
         List<Object[]> list = new ArrayList<>();
-        list.add(new Object[] { new GroupData().withName("test 1").withHeader("test header 1").withFooter("test footer 1")});
-        list.add(new Object[] { new GroupData().withName("test 2").withHeader("test header 2").withFooter("test footer 2")});
-        list.add(new Object[] { new GroupData().withName("test 3").withHeader("test header 3").withFooter("test footer 3")});
+        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.csv")));
+        String line = reader.readLine();
+        while (line != null) {
+            String[] groupField = line.split(";");
+            list.add(new Object[]{new GroupData().withName(groupField[0]).withHeader(groupField[1]).withFooter(groupField[2])});
+            line = reader.readLine();
+        }
+        reader.close();
         return list.iterator();
     }
 
@@ -27,7 +33,6 @@ public class GroupCreationTests extends TestBase {
     public void testGroupCreation(GroupData group) {
         app.goTo().groupPage();
         Groups before = app.group().all();
-        //GroupData group = new GroupData().withName("test1");
         app.group().create(group);
         assertThat(app.group().count(), equalTo(before.size() + 1));
         Groups after = app.group().all();
