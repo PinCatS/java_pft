22526beeb7aaf2de022a75c90091aa0da0a345f8
Sergey <pincats@yandex.ru>
2016-11-25 02:37:31 +0300

    Obtain ContactData from db

--- addressbook-web-tests/src/test/java/ru/pincats/jpt/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/pincats/jpt/addressbook/model/ContactData.java
@@ -3,44 +3,95 @@ package ru.pincats.jpt.addressbook.model;
 import com.google.gson.annotations.Expose;
 import com.thoughtworks.xstream.annotations.XStreamAlias;
 import com.thoughtworks.xstream.annotations.XStreamOmitField;
+import org.hibernate.annotations.Type;
 
+import javax.persistence.*;
 import java.io.File;
 
 @XStreamAlias("contact")
+@Entity
+@Table(name = "addressbook")
 public class ContactData {
     @XStreamOmitField
+    @Id
+    @Column(name = "id")
     private int id;
+
     @Expose
+    @Column(name = "firstname")
     private String firstName;
+
     @Expose
+    @Column(name = "lastname")
     private String lastName;
+
     @Expose
+    @Column(name = "nickname")
     private String nickname;
+
     @Expose
+    @Column(name = "title")
     private String title;
+
     @Expose
+    @Column(name = "company")
     private String company;
+
     @Expose
+    @Column(name = "address")
+    @Type(type = "text")
     private String postAddress;
+
     @Expose
+    @Column(name = "email")
+    @Type(type = "text")
     private String email;
+
     @Expose
+    @Column(name = "email2")
+    @Type(type = "text")
     private String email2;
+
     @Expose
+    @Column(name = "email3")
+    @Type(type = "text")
     private String email3;
+
     @Expose
+    @Column(name = "home")
+    @Type(type = "text")
     private String homePhone;
+
     @Expose
+    @Column(name = "phone2")
+    @Type(type = "text")
     private String homePhone2;
+
     @Expose
+    @Column(name = "mobile")
+    @Type(type = "text")
     private String mobilePhone;
+
     @Expose
+    @Column(name = "work")
+    @Type(type = "text")
     private String workPhone;
+
+    @Transient
     private String allPhones;
+
+    @Transient
     private String allEmails;
+
     @Expose
+    @Transient
     private String group;
-    private File photo;
+
+    @Column(name = "photo")
+    @Type(type = "text")
+    private String photo;
+
+    @Transient
     private String allDetails;
 
     public ContactData withId(int id) {
@@ -79,7 +130,7 @@ public class ContactData {
     }
 
     public ContactData withPhoto(File photo) {
-        this.photo = photo;
+        this.photo = photo.getPath();
         return this;
     }
 
@@ -168,7 +219,12 @@ public class ContactData {
     }
 
     public File getPhoto() {
-        return photo;
+        if (photo == null) {
+            return null;
+        }
+        else {
+            return new File(photo);
+        }
     }
 
     public String getHomePhone() {
---
+++ sandbox/src/main/java/ru/pincats/jpt/sandbox/StringTest.java
@@ -0,0 +1,17 @@
+package ru.pincats.jpt.sandbox;
+
+/**
+ * Created by PinCatS on 25.11.2016.
+ */
+public class StringTest {
+    private String name;
+    private String name2 = new String();
+
+    public String getName() {
+        return name;
+    }
+
+    public String getName2() {
+        return name2;
+    }
+}
